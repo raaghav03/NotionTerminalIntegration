0160d9497c729055bc0fddeada2b7f3cfaec1627
raaghav03 <raghavnagpal2003@gmail.com>
2024-03-18 22:50:09 +0530

    bug fix in data retrieval

--- index.js
+++ index.js
@@ -21,45 +21,47 @@ async function listDatabaseItems() {
 
   response.results.forEach((page) => {
     const todoItem = page.properties["Todo Item"].title[0].plain_text;
-    const priorityLevel = page.properties["Priority"].select["name"];
+    const priorityLevel = page.properties["Priority"].select.name;
     const taskDate = page.properties["Due Date"].date["start"];
-
-    // console.log(
-    //   `task name is ${todoItem} and priority level is ${priorityLevel}`
-    // );
+    console.log(
+      `${todoItem} is a ${priorityLevel} priority and has to be done by ${taskDate}`
+    );
   });
 }
-async function addItemstoDatabase() {
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  });
 
-  response.results.forEach((page) => {
-    const todoItem = page.properties["Todo Item"].title;
-    // console.log(todoItem);
-  });
-  rl.question("What is the task you want to add ? \n ", async (taskName) => {
-    try {
-      const addItem = await notion.pages.create({
-        parent: { database_id: process.env.NOTION_DATABASE_ID },
-        properties: {
-          "Todo Item": {
-            title: [
-              {
-                text: {
-                  content: taskName,
-                },
-              },
-            ],
-          },
-        },
-      });
-      console.log(`${taskName} is added to the db`);
-    } catch (error) {
-      console.error("Error adding the task to the database:", error);
-    } finally {
-      rl.close(); // Ensure readline is closed after the operation
-    }
-  });
-}
-addItemstoDatabase();
+listDatabaseItems();
+
+// async function addItemstoDatabase() {
+//   const response = await notion.databases.query({
+//     database_id: process.env.NOTION_DATABASE_ID,
+//   });
+
+//   response.results.forEach((page) => {
+//     const todoItem = page.properties["Todo Item"].title;
+//     // console.log(todoItem);
+//   });
+//   rl.question("What is the task you want to add ? \n ", async (taskName) => {
+//     try {
+//       const addItem = await notion.pages.create({
+//         parent: { database_id: process.env.NOTION_DATABASE_ID },
+//         properties: {
+//           "Todo Item": {
+//             title: [
+//               {
+//                 text: {
+//                   content: taskName,
+//                 },
+//               },
+//             ],
+//           },
+//         },
+//       });
+//       console.log(`${taskName} is added to the db`);
+//     } catch (error) {
+//       console.error("Error adding the task to the database:", error);
+//     } finally {
+//       rl.close(); // Ensure readline is closed after the operation
+//     }
+//   });
+// }
+// addItemstoDatabase();
